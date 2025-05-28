0b6af657eb088216089727dcf9fb549399f3a439
jun <ryuu.public@gmail.com>
2025-05-29 02:28:25 +0900

    fix: remove ambiguous api

--- src/main/java/com/ratelo/blog/api/UserController.java
+++ src/main/java/com/ratelo/blog/api/UserController.java
@@ -3,11 +3,9 @@ package com.ratelo.blog.api;
 import com.ratelo.blog.domain.user.User;
 import com.ratelo.blog.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,11 +18,6 @@ public class UserController {
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
-    }
-
-    @GetMapping("/{username}")
-    public User getUserByUsername(@PathVariable String username) {
-        return userService.getUserByUsername(username);
     }
 
     @GetMapping

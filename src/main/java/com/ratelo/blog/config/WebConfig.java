978da884f6024eb68faa7662bb8d8e2bf8a9999e
jun <ryuu.public@gmail.com>
2025-05-29 02:48:59 +0900

    feat: CORS config

---
+++ src/main/java/com/ratelo/blog/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ratelo.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000", "https://hyeongjun.me")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

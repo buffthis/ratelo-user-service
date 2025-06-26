171695ce1620d592d7f02b2d62a243972890d740
jun <ryuu.public@gmail.com>
2025-06-27 00:31:58 +0900

    feat: modify config

--- src/main/java/com/ratelo/blog/config/WebConfig.java
+++ src/main/java/com/ratelo/blog/config/WebConfig.java
@@ -1,17 +1,27 @@
 package com.ratelo.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "https://unblind.kr", "https://hyeongjun.me")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://unblind.kr",
+                            "https://hyeongjun.me",
+                            "http://localhost:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }

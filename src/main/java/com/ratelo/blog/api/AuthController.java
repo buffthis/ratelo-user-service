0b44549ce2394ee1d693abd4d4ad7e7bc2df596f
jun <ryuu.public@gmail.com>
2025-07-13 00:29:19 +0900

    chore: optimize imports

--- src/main/java/com/ratelo/blog/api/AuthController.java
+++ src/main/java/com/ratelo/blog/api/AuthController.java
@@ -1,22 +1,20 @@
 package com.ratelo.blog.api;
 
+import com.ratelo.blog.domain.user.User;
 import com.ratelo.blog.dto.user.UserLoginRequest;
+import com.ratelo.blog.dto.user.UserSimpleResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.*;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import com.ratelo.blog.domain.user.User;
-import com.ratelo.blog.dto.user.UserSimpleResponse;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {

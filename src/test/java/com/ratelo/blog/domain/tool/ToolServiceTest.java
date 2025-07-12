04341f538efd7b7ebfc44896be52a0f0940a36a1
jun <ryuu.public@gmail.com>
2025-07-13 00:53:09 +0900

    test: add fixtures and test service logic

--- src/test/java/com/ratelo/blog/domain/tool/ToolServiceTest.java
+++ src/test/java/com/ratelo/blog/domain/tool/ToolServiceTest.java
@@ -27,11 +27,11 @@ public class ToolServiceTest {
     @DisplayName("Tool이 존재하면 해당 Tool을 반환한다")
     void shouldReturnTool_whenToolExists() {
         // given
-        Tool tool = ToolFixture.createDefaultTool();
+        final Tool tool = ToolFixture.createDefaultTool();
         when(toolRepository.findById(tool.getId())).thenReturn(Optional.of(tool));
 
         // when
-        Tool result = toolService.getToolById(tool.getId());
+        final Tool result = toolService.getToolById(tool.getId());
 
         // then
         assertNotNull(result);
@@ -42,11 +42,11 @@ public class ToolServiceTest {
     @DisplayName("Tool이 존재하지 않으면 null을 반환한다")
     void shouldReturnNull_whenToolDoesNotExist() {
         // given
-        Long notExistToolId = 1L;
+        final Long notExistToolId = 1L;
         when(toolRepository.findById(notExistToolId)).thenReturn(Optional.empty());
 
         // when
-        Tool result = toolService.getToolById(notExistToolId);
+        final Tool result = toolService.getToolById(notExistToolId);
 
         // then
         assertNull(result);
